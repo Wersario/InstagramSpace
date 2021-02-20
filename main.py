05c0e05e01352a289fae67ad3db3461aadc7ff2b
Wersario <60400132+Wersario@users.noreply.github.com>
2021-02-20 23:55:22 +0300

    Added ifmain

--- main.py
+++ main.py
@@ -6,15 +6,17 @@ from instabot import Bot
 from os import listdir
 
 
-load_dotenv()
-if os.path.exists('config'):
-    shutil.rmtree('config')
-dir_name = 'images'
-bot = Bot()
-bot.login(username=os.getenv("INSTA_LOGIN"), password=os.getenv("INSTA_PASSWORD"))
+if __name__ == '__main__':
 
-for number, photo_path in enumerate(listdir(dir_name)):
-    bot.upload_photo(f'{dir_name}/{photo_path}', caption=f'Beautiful space photo number {number}')
+    load_dotenv()
+    if os.path.exists('config'):
+        shutil.rmtree('config')
+    dir_name = 'images'
+    bot = Bot()
+    bot.login(username=os.getenv("INSTA_LOGIN"), password=os.getenv("INSTA_PASSWORD"))
 
-for item in os.listdir(dir_name):
-    os.remove(f'{dir_name}/{item}')
+    for number, photo_path in enumerate(listdir(dir_name)):
+        bot.upload_photo(f'{dir_name}/{photo_path}', caption=f'Beautiful space photo number {number}')
+
+    for item in os.listdir(dir_name):
+        os.remove(f'{dir_name}/{item}')
