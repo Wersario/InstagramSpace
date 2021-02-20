f2fe75c6852e8810cf78fa430b0bf3923c384691
Wersario <60400132+Wersario@users.noreply.github.com>
2021-02-20 23:17:55 +0300

    Separated functions

--- fetch_spacex.py
+++ fetch_spacex.py
@@ -1,27 +1,9 @@
 import os
+
 import requests
-from PIL import Image
 
-
-def get_link_extension(link):
-    separated_link = link.split('.')
-    return '.' + separated_link[-1]
-
-
-def resize_picture(path):
-    image = Image.open(path)
-    image.thumbnail((1080, 1080))
-    image.convert('RGB').save(f'{path.split(".")[0]}.jpg')
-    if get_link_extension(path) != '.jpg':
-        os.remove(path)
-
-
-def download_image(url, path):
-    os.makedirs("images", exist_ok=True)
-    response = requests.get(url, verify=False)
-    response.raise_for_status()
-    with open(path, "wb") as file:
-        file.write(response.content)
+from utils import resize_picture
+from utils import download_image
 
 
 def fetch_spacex_last_launch():
---
+++ utils.py
@@ -0,0 +1,27 @@
+import os
+
+import requests
+
+from PIL import Image
+from urllib.parse import urlparse
+
+
+def get_link_extension(link):
+    separated_link = urlparse(link).path.split('.')
+    return '.' + separated_link[-1]
+
+
+def resize_picture(path):
+    image = Image.open(path)
+    image.thumbnail((1080, 1080))
+    image.convert('RGB').save(f'{path.split(".")[0]}.jpg')
+    if get_link_extension(path) != '.jpg':
+        os.remove(path)
+
+
+def download_image(url, path):
+    os.makedirs("images", exist_ok=True)
+    response = requests.get(url, verify=False)
+    response.raise_for_status()
+    with open(path, "wb") as file:
+        file.write(response.content)
