bfa486da2edc0855bc3d254d76b9463e361e33a7
Wersario <60400132+Wersario@users.noreply.github.com>
2021-02-23 18:31:27 +0300

    Little changes

--- fetch_spacex.py
+++ fetch_spacex.py
@@ -1,15 +1,14 @@
-import os
-
 import requests
 
-from utils import resize_picture
+from utils import create_directory
 from utils import download_image
+from utils import resize_picture
 
 
-def fetch_spacex_last_launch():
-    os.makedirs("images", exist_ok=True)
+def fetch_spacex_last_launch(dir_name):
+    create_directory(dir_name)
     response = requests.get("https://api.spacexdata.com/v4/launches/latest")
     response.raise_for_status()
     for count, link in enumerate(response.json()['links']['flickr']['original']):
-        download_image(link, f'images/spacex{count}.jpg')
-        resize_picture(f'images/spacex{count}.jpg')
+        download_image(link, f'{dir_name}/spacex{count}.jpg')
+        resize_picture(f'{dir_name}/spacex{count}.jpg')
