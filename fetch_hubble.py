9ec4d8db2867553c6072b9d47eff983d340a5c28
Wersario <60400132+Wersario@users.noreply.github.com>
2021-02-23 20:27:32 +0300

    Added dir_name param to functions

--- fetch_hubble.py
+++ fetch_hubble.py
@@ -12,7 +12,7 @@ def fetch_hubble_id_photo(image_id, number, dir_name):
     response = requests.get(url, verify=False)
     response.raise_for_status()
     image = response.json()['image_files'][-1]
-    download_image(f"https:{image['file_url']}", f"{dir_name}/hubble{image_id}_{number}{get_link_extension(image['file_url'])}")
+    download_image(f"https:{image['file_url']}", f"{dir_name}/hubble{image_id}_{number}{get_link_extension(image['file_url'])}", dir_name)
     resize_picture(f"{dir_name}/hubble{image_id}_{number}{get_link_extension(image['file_url'])}")
 
 
--- fetch_spacex.py
+++ fetch_spacex.py
@@ -10,5 +10,5 @@ def fetch_spacex_last_launch(dir_name):
     response = requests.get("https://api.spacexdata.com/v4/launches/latest")
     response.raise_for_status()
     for count, link in enumerate(response.json()['links']['flickr']['original']):
-        download_image(link, f'{dir_name}/spacex{count}.jpg')
+        download_image(link, f'{dir_name}/spacex{count}.jpg', dir_name)
         resize_picture(f'{dir_name}/spacex{count}.jpg')
