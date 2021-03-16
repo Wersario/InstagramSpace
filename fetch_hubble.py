fad319c09a08dc5ca51b64372a7a5be0020f32ad
Wersario <60400132+Wersario@users.noreply.github.com>
2021-03-16 19:25:37 +0300

    Added __all__, added new import

--- fetch_hubble.py
+++ fetch_hubble.py
@@ -1,22 +1,22 @@
 import requests
 
-from utils import create_directory
 from utils import download_image
 from utils import get_link_extension
 from utils import resize_picture
+from utils import save_picture_as_jpg
 
+__all__ = ['download_image', 'get_link_extension', 'resize_picture', 'save_picture_as_jpg']
 
 def fetch_hubble_id_photo(image_id, number, dir_name):
     url = f'http://hubblesite.org/api/v3/image/{image_id}'
     response = requests.get(url, verify=False)
     response.raise_for_status()
     image = response.json()['image_files'][-1]
-    download_image(f"https:{image['file_url']}", f"{dir_name}/hubble{image_id}_{number}{get_link_extension(image['file_url'])}", dir_name)
-    resize_picture(f"{dir_name}/hubble{image_id}_{number}{get_link_extension(image['file_url'])}")
+    download_image(f"https:{image['file_url']}", f"{dir_name}/hubble{image_id}_{number}{get_link_extension(image['file_url'])}")
+    resize_picture(save_picture_as_jpg(f"{dir_name}/hubble{image_id}_{number}{get_link_extension(image['file_url'])}"))
 
 
 def fetch_hubble_collection(collection_name, dir_name):
-    create_directory(dir_name)
     url = f'http://hubblesite.org/api/v3/image/{collection_name}'
     response = requests.get(url, verify=False)
     response.raise_for_status()
