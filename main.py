3edd6dc1d9df563533bf2fde22b74a3e3b81d663
Wersario <60400132+Wersario@users.noreply.github.com>
2021-03-16 19:26:22 +0300

    Added __all__, added downloading pictures

--- main.py
+++ main.py
@@ -5,19 +5,28 @@ import shutil
 from dotenv import load_dotenv
 from instabot import Bot
 from os import listdir
+from fetch_hubble import fetch_hubble_collection
+from fetch_spacex import fetch_spacex_last_launch
 
+__all__ = ['fetch_hubble_collection', 'fetch_spacex_last_launch']
 
 if __name__ == '__main__':
 
     load_dotenv()
+    dir_name = 'images'
+    collection_name = 'holiday_cards'
+
+    os.makedirs(dir_name, exist_ok=True)
+    fetch_spacex_last_launch(dir_name)
+    fetch_hubble_collection(collection_name, dir_name)
+
     if os.path.exists('config'):
         shutil.rmtree('config')
-    dir_name = 'images'
     bot = Bot()
-    bot.login(username=os.getenv("INSTA_LOGIN"), password=os.getenv("INSTA_PASSWORD"))
+    bot.login(username=os.getenv('INSTA_LOGIN'), password=os.getenv('INSTA_PASSWORD'))
 
     for number, photo_path in enumerate(listdir(dir_name)):
         bot.upload_photo(f'{dir_name}/{photo_path}', caption=f'Beautiful space photo number {number}')
 
     for item in os.listdir(dir_name):
-        os.remove(f'{dir_name}/{item}')
+        shutil.rmtree(f'{dir_name}/{item}')
