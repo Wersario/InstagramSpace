7b1a2372932a5197ddf40623cbc72094ba3699d9
Wersario <60400132+Wersario@users.noreply.github.com>
2021-03-16 19:26:58 +0300

    Added new fucntion, deleted create_directory function. changed resize_picture

--- utils.py
+++ utils.py
@@ -6,8 +6,11 @@ from PIL import Image
 from urllib.parse import urlparse
 
 
-def create_directory(dir_name):
-    os.makedirs(dir_name, exist_ok=True)
+def save_picture_as_jpg(path):
+    image = Image.open(path)
+    image.convert('RGB').save(f'{os.path.splitext(path)[0]}.jpg')
+    if get_link_extension(path) != '.jpg':
+        os.remove(path)
 
 
 def get_link_extension(link):
@@ -18,14 +21,10 @@ def get_link_extension(link):
 def resize_picture(path):
     image = Image.open(path)
     image.thumbnail((1080, 1080))
-    image.convert('RGB').save(f'{os.path.splitext(path)[0]}.jpg')
-    if get_link_extension(path) != '.jpg':
-        os.remove(path)
 
 
-def download_image(url, path, dir_name):
-    create_directory(dir_name)
+def download_image(url, path):
     response = requests.get(url, verify=False)
     response.raise_for_status()
-    with open(path, "wb") as file:
+    with open(path, 'wb') as file:
         file.write(response.content)
