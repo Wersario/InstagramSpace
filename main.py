2ededb2a276de79d0619d6c56474ddb34af5f70e
Wersario <60400132+Wersario@users.noreply.github.com>
2021-04-10 15:03:41 +0300

    Added "try, finally"

--- main.py
+++ main.py
@@ -15,23 +15,27 @@ if __name__ == '__main__':
     load_dotenv()
     dir_name = 'images'
     collection_name = 'spacecraft'
-
     os.makedirs(dir_name, exist_ok=True)
-    fetch_spacex_last_launch(dir_name)
-    fetch_hubble_collection(collection_name, dir_name)
 
-    for photo in os.listdir(dir_name):
-        save_picture_as_jpg(f'{dir_name}/{photo}')
-        resize_picture(f'{dir_name}/{f"{os.path.splitext(photo)[0]}.jpg"}')
+    try:
 
-    if os.path.exists('config'):
-        shutil.rmtree('config')
-    bot = Bot()
-    bot.login(username=os.getenv('INSTA_LOGIN'), password=os.getenv('INSTA_PASSWORD'))
+        fetch_spacex_last_launch(dir_name)
+        fetch_hubble_collection(collection_name, dir_name)
 
-    for number, photo_path in enumerate(listdir(dir_name)):
-        bot.upload_photo(f'{dir_name}/{photo_path}', caption=f'Beautiful space photo number {number}')
+        for photo in os.listdir(dir_name):
+            save_picture_as_jpg(f'{dir_name}/{photo}')
+            resize_picture(f'{dir_name}/{os.path.splitext(photo)[0]}.jpg')
 
-    for item in os.listdir(dir_name):
-        shutil.rmtree(f'{dir_name}/{item}')
+        bot = Bot()
+        bot.login(username=os.getenv('INSTA_LOGIN'), password=os.getenv('INSTA_PASSWORD'))
+
+        for number, photo_path in enumerate(listdir(dir_name)):
+            bot.upload_photo(f'{dir_name}/{photo_path}', caption=f'Beautiful space photo number {number}')
+
+    finally:
+
+        for item in os.listdir(dir_name):
+            shutil.rmtree(f'{dir_name}/{item}')
+        if os.path.exists('config'):
+            shutil.rmtree('config')
 
