207cff123adaf4925fe5a2e3f13a4487862ba52a
Wersario <60400132+Wersario@users.noreply.github.com>
2021-04-10 15:02:44 +0300

    Added fix_link

--- fetch_hubble.py
+++ fetch_hubble.py
@@ -2,15 +2,15 @@ import requests
 
 from utils import download_image
 from utils import get_link_extension
+from utils import fix_link
 
 
 def fetch_hubble_id_photo(image_id, number, dir_name):
     url = f'https://hubblesite.org/api/v3/image/{image_id}'
     response = requests.get(url, verify=False)
     response.raise_for_status()
-    image = response.json()['image_files'][-1]
-    download_image(f"https:{image['file_url']}", f"{dir_name}/hubble{image_id}_{number}{get_link_extension(image['file_url'])}")
-    print(f"https:{image['file_url']}", 'downloaded')
+    image = fix_link(response.json()['image_files'][-1]['file_url'])
+    download_image(f"https:{image}", f"{dir_name}/hubble{image_id}_{number}{get_link_extension(image)}")
 
 
 def fetch_hubble_collection(collection_name, dir_name):
--- fetch_spacex.py
+++ fetch_spacex.py
@@ -1,10 +1,12 @@
 import requests
 
 from utils import download_image
+from utils import fix_link
 
 
 def fetch_spacex_last_launch(dir_name):
     response = requests.get('https://api.spacexdata.com/v3/launches/64')
     response.raise_for_status()
     for count, link in enumerate(response.json()['links']['flickr_images']):
-        download_image(link, f'{dir_name}/spacex{count}.jpg')
+        fixed_link = fix_link(link)
+        download_image(fixed_link, f'{dir_name}/spacex{count}.jpg')
